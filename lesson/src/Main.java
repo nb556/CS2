e9b559657fc80a60827183ba8218adfa6998d4ba
nb556 <nicklas.bouchard@gmail.com>
2022-10-07 09:57:47 +0300

    CS2

--- lesson/src/Main.java
+++ lesson/src/Main.java
@@ -2,14 +2,31 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int key = 2;
-        int[] array = {5, 4, 3, 2, 1};
-        boolean found = false;
+        Reservation[] reservations = new Reservation[3];
+        reservations [0] = new Reservation("John", 15);
+        reservations [0] = new Reservation("Jane", 12);
+        reservations [0] = new Reservation("Wendy", 15);
 
-        for (int index = 0; !found && index < array.length; index++)
-            if (array[index] == key)
-                found = true;
-
-        System.out.println(found);
+        String key = "Mary";
+        for (Reservation i : reservations)
+        {
+            if (i.name.equals (key))
+                {
+                System.out.println(i.room);
+                break;
+            }
+        }
     }
+}
+
+class Reservation
+{
+    Reservation (String name, int room)
+    {
+        this.name = name;
+        this.room = room;
+    }
+
+    String name;
+    int room;
 }
