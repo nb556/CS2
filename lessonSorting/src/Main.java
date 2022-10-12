6181a2071b0d78b16e6a2fa3cd52533185aaec70
nb556 <nicklas.bouchard@gmail.com>
2022-10-12 13:30:15 +0300

    CS2

---
+++ lessonSorting/src/Main.java
@@ -0,0 +1,30 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] data = {5, 11, 43, 12, 55};
+
+        int lo;
+
+        for (int i = 0; i <= data.length - 2; i++)
+        {
+            int indSmallest = i;
+            for (int j = i + 1; j < data.length; j++)
+            {
+                if (data[indSmallest] > data[j])
+                    indSmallest = data[j];
+            }
+            swap(data, i, indSmallest);
+        }
+        for (int i : data)
+            System.out.print(i);
+    }
+
+    static  void swap (int[] arr, int i, int j)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
+
