80e4ae5121ae8de992a0eac66663678ea2fe536d
nb556 <nicklas.bouchard@gmail.com>
2022-10-12 13:02:47 +0300

    CS2

--- Array algorithms 2/src/Main.java
+++ Array algorithms 2/src/Main.java
@@ -2,6 +2,22 @@ public class Main
 {
     public static void main(String[] args)
     {
-        String[] Students
+        Students[] student = new Students[4];
+        student[0] = new Students("Alissa", "B");
+        student[1] = new Students("Ben", "D");
+        student[2] = new Students("Charlie", "B");
+        student[3] = new Students("Dianna", "A");
     }
+}
+
+
+
+class Students
+{
+    Students (String name, String grade)
+    {
+        this.name = name;
+        this.grade = grade;
+    }
+    String name; String grade;
 }
