1ce423116ac8d97d0626bcf8ddfe8e58714156ab
nb556 <nicklas.bouchard@gmail.com>
2022-10-28 09:58:49 +0300

    CS2

---
+++ Lists 2/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next ();
+        if (word.equals("!"))
+            System.exit(0);
+    }
+}
