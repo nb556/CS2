fce68503874ec326159e91032bbdba819faa90dd
nb556 <nicklas.bouchard@gmail.com>
2022-10-28 09:53:09 +0300

    CS2

--- Lists 1/src/Main.java
+++ Lists 1/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.LinkedList;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        LinkedList<Integer> list = new LinkedList<>();
 
+        list.add(1);
+        list.addLast(6);
+        list.add(1, 0);
+        int i = 0;
+        while (list.get (i) != 6)
+            i++;
+        list.remove(i);
+        list.removeFirst();
+
+        System.out.println(list);
     }
 }
 
