927e70f46574c67cb0c8607f5609643b74188069
nb556 <nicklas.bouchard@gmail.com>
2022-11-09 12:24:15 +0200

    CS2

--- Class relationships 3/src/Project.java
+++ Class relationships 3/src/Project.java
@@ -1,32 +1,23 @@
-public class Project
-{
+public class Project {
     int id;
     int taskAmount;
-    public Project(int id, int taskAmount)
-    {
+    public Project(int id, int taskAmount) {
         this.id = id;
         this.taskAmount = taskAmount;
     }
 }
-
-class Task
-{
+class Task {
     String name;
     int workerAmount;
-    Task(String name, int workerAmount)
-    {
+    Task(String name, int workerAmount) {
         this.name = name;
         this.workerAmount = workerAmount;
     }
 }
-
-class Worker
-{
+class Worker {
     String name;
     boolean isEmployee;
-
-    Worker(String name, boolean isEmployee)
-    {
+    Worker(String name, boolean isEmployee) {
         this.name = name;
         this.isEmployee = isEmployee;
     }
