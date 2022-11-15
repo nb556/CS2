ac60b4b5a36bb918c85a13c8ab7938c6489ddf7a
nb556 <nicklas.bouchard@gmail.com>
2022-11-15 14:16:32 +0200

    CS2

--- CS2 Project/src/Main.java
+++ CS2 Project/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         ArrayList<String> list = new ArrayList<>(Arrays.asList(words));
 
         while (true){
-            System.out.println(clear + "To start a task, enter 'sort' or 'search'");
+            System.out.println(clear + "To start a task, enter 'sort', 'search' or 'list'");
             String prompt = scanner.next();
             if (prompt.equals("stop") || prompt.equals("!") || prompt.equals("break"))
                 break;
@@ -26,8 +26,11 @@ public class Main {
                 }
             } else if (prompt.equals("sort")) {
                 BubbleSort.sortString(list);
+                System.out.println("\n\nSorted");
+                scanner.next();
+            } else if (prompt.equals("list")) {
                 for (String i : list)
-                    System.out.print(" " + i);
+                    System.out.print(i + ", ");
                 scanner.next();
             }
         }
