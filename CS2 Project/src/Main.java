032fe8f97415d66d20f7b5c3b276dac7dfedc211
nb556 <nicklas.bouchard@gmail.com>
2022-11-15 14:13:10 +0200

    CS2

--- CS2 Project/src/Main.java
+++ CS2 Project/src/Main.java
@@ -5,18 +5,18 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws  Exception{
         Scanner scanner = new Scanner(System.in);
+        String clear = ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         String[] words = {"klingon", "belfast", "xavier",  "abacus", "chronological", "arkansas", "compile"};
         ArrayList<String> list = new ArrayList<>(Arrays.asList(words));
-        System.out.println(list.size());
 
         while (true){
-            System.out.println("\n\n\n\n\n\nTo start a task, enter 'sort' or 'search'");
+            System.out.println(clear + "To start a task, enter 'sort' or 'search'");
             String prompt = scanner.next();
             if (prompt.equals("stop") || prompt.equals("!") || prompt.equals("break"))
                 break;
             if (prompt.equals("search")) {
                 while (true) {
-                    System.out.println("\n\n\nEnter 'back' to go back\n\n");
+                    System.out.println(clear + "Enter 'back' to go back\n\n");
                     System.out.println("Please enter a keyword");
                     prompt = scanner.next();
                     if (prompt.equals("back"))
